e02d8ac170f643a27108eb331dde3f29638f6230
ilmi <ilmir311@gmail.com>
2020-12-22 09:20:26 +0800

    Progress bar and asynctask

--- app/src/main/java/com/example/threads/MainActivity.java
+++ app/src/main/java/com/example/threads/MainActivity.java
@@ -9,10 +9,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
     EditText editText;
+    ProgressBar pb;
     Handler myhandler = new Handler();
 
     @Override
@@ -21,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         button = findViewById(R.id.button);
         editText = findViewById(R.id.editText);
+        pb=findViewById(R.id.progressBar);
+        
         Log.println(Log.INFO, "ThreadID", "thread ID:" + String.valueOf(Thread.currentThread().getId()));
     }
 
@@ -34,13 +38,26 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private class myAsyncTask extends AsyncTask<Void,Void,Void> {
+    private class myAsyncTask extends AsyncTask<Void,Integer,Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pb.setMax(100);
+            pb.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            pb.setProgress(values[0]);
+        }
 
         @Override
         protected Void doInBackground(Void... voids) {
-            for (int i = 0; i < 5; i++) {
+            for (int i = 0; i < 100; i++) {
+                publishProgress(i);
                 try {
-                    Thread.sleep(1000);
+                    Thread.sleep(100);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -52,15 +69,16 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
-            button.setText(editText.getText());
+            button.setText("task completed"+editText.getText());
+            pb.setVisibility(View.INVISIBLE);
         }
     }
 
     public class myThread extends Thread {
         public void run() {
-            for (int i = 0; i < 5; i++) {
+            for (int i = 0; i < 100; i++) {
                 try {
-                    Thread.sleep(1000);
+                    Thread.sleep(100);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
